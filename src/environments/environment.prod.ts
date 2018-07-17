c77da4503c40658608cddc379a45366fa38684e4
Jack <jhegarty14@yahoo.com>
2018-07-16 23:29:42 -0700

    configured heroku settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  envUrl: 'https://foresight-challenge.herokuapp.com',
+  apiUrl: 'https://foresight-challenge.herokuapp.com/api'
 };
