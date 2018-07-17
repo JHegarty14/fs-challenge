5cd28e59d8d09cb041ea699cb02f606901971607
Jack <jhegarty14@yahoo.com>
2018-07-16 23:07:10 -0700

    configured heroku settings

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let timerId = null,
 
 app.use(express.static(__dirname + '/dist')); 
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
